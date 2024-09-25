3cff9df76c97ac2f34e9406cb30c110ec16d79f4
ChristianSchool <Christian.cromer@snhu.edu>
2024-09-25 12:22:04 -0500

    Commit for module 4

---
+++ travlr/app_server/models/seed.js
@@ -0,0 +1,16 @@
+const Mongoose = require('./db.js');
+const Trip = require('./travlr.js');
+
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+const seedDB = async () => {
+    await Trip.deleteMany({});
+    await Trip.insertMany(trips);
+
+};
+
+seedDB().then(async () => {
+    await Mongoose.connection.close();
+    process.exit(0);
+})
